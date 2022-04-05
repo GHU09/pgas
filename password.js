331e609aaeb726615730396484732044b6c59926
azarja-alt <103034535+azarja-alt@users.noreply.github.com>
2022-04-05 13:46:00 +0200

    Update password.js

--- password.js
+++ password.js
@@ -4,6 +4,6 @@ var pass1="W8w00rd!";
 password=prompt('Enter Password',' '); 
 if (password==pass1) 
 alert('Correct Password! Click OK to Enter!'); 
-else { window.location="http://https://ghu09.github.io/pgas/"; 
+else { window.location="http://https://.github.io/"; 
 } 
 </SCRIPT>
