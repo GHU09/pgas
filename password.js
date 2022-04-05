2fc83466d4ed7ea78bdb4670bedcdebcdef6e7a5
Samuël <78428255+GHU09@users.noreply.github.com>
2022-04-05 14:35:18 +0200

    Delete password.js

--- password.js
+++
@@ -1,9 +0,0 @@
-var userResponse = "N/A";
-
-function askPass() {
-    "use strict";
-    userResponse = prompt("What's the password?", "testv1");
-    if (userResponse === "SpeculAcessPls") {
-        window.open("https://ghu09.github.io/pgas/");
-    }
-}
